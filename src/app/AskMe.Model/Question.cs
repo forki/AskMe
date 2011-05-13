70f0fbc07952cd5f550ae3975becba1b451d1e82
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-05-13 12:37:04 +0200

    Added MSpec

---
+++ src/app/AskMe.Model/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace AskMe.Model
+{
+    public class Question
+    {
+        public Question(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; private set; }
+    }
+}
---
+++ src/app/AskMe.TextParser/Parser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using AskMe.Model;
+
+namespace AskMe.TextParser
+{
+    public class Parser
+    {
+        public List<Question> Parse(string text)
+        {
+            return new List<Question>{new Question(text)};
+        }
+    }
+}
