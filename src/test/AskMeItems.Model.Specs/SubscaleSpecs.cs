c63b4bb8df7b30a27d8941c1235054bad884dfc7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-05-17 21:48:34 +0200

    Finding subscales

--- src/test/AskMeItems.Model.Specs/SubscaleSpecs.cs
+++ src/test/AskMeItems.Model.Specs/SubscaleSpecs.cs
@@ -9,8 +9,20 @@ namespace AskMeItems.Model.Specs
     {
         static List<Subscale> Subscales;
         Because of = () => Subscales = Questionnaire.GetSubscales();
-        It should_contain_2_scales = () => Subscales.Count.ShouldEqual(2);
-        It should_contain_one_subscale = () => Subscales.Select(s => s.Name).ShouldContain("HADS");
-        It should_contain_the_mainscale = () => Subscales.Select(s => s.Name).ShouldContain("");
+
+        It should_contain_2_scales =
+            () => Subscales.Count.ShouldEqual(2);
+
+        It should_contain_one_subscale =
+            () => Subscales.Select(s => s.Name).ShouldContain("HADS");
+
+        It should_contain_both_items_in_the_subscale =
+            () => Subscales.Where(s => s.Name == "HADS").First().Items.Count.ShouldEqual(2);
+
+        It should_contain_both_items_in_the_mainscale =
+            () => Subscales.Where(s => s.Name == "").First().Items.Count.ShouldEqual(2);
+
+        It should_contain_the_mainscale = 
+            () => Subscales.Select(s => s.Name).ShouldContain("");
     }
 }
